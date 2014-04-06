a83b12f5c0a35006d06bf2f10be653a69147862d
Trabajador1 <trabajador@egit.com>
2014-04-06 20:32:47 +0200

    Subida desde el trabajador

--- GitHubTutorial/src/GitHub.java
+++ GitHubTutorial/src/GitHub.java
@@ -4,7 +4,8 @@ public class GitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("Proyecto del creador");
-
+		System.out.println("Linea agregada por el trabajador");
+		
 	}
 
 }
